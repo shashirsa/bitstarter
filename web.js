2e980af6d1c220b77a15f395a308a45259403c46
shashirsa <ubuntu@ip-10-245-65-105.ec2.internal>
2013-07-15 06:21:34 +0000

    adding web.js and index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync("index.html");
+var content = data.toString("utf8");
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
